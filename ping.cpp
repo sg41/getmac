1761224692cc4984b09b7eebab7d15dbd14663bc
Сергей Горбатенко <gorbatenkosy@ema.ru>
2025-07-05 18:45:24 +0700

    Iteration 2

--- ping.cpp
+++ ping.cpp
@@ -13,7 +13,6 @@
 #include <cstring>
 #include <ctime>
 
-// Класс для работы с ICMP
 class ICMPMacResolver {
  public:
   ICMPMacResolver() : sock_raw(-1) {}
@@ -30,7 +29,6 @@ class ICMPMacResolver {
  private:
   int sock_raw;
 
-  // Вычисление контрольной суммы ICMP пакета
   unsigned short checksum(void* b, int len) {
     unsigned short* buf = static_cast<unsigned short*>(b);
     unsigned int sum = 0;
@@ -47,19 +45,17 @@ class ICMPMacResolver {
   }
 
   bool init_socket() {
-    sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    // Используем SOCK_RAW с IPPROTO_ICMP вместо AF_PACKET
+    sock_raw = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sock_raw < 0) {
       perror("socket");
       return false;
     }
 
-    struct ifreq ifr;
-    std::memset(&ifr, 0, sizeof(ifr));
-    std::strncpy(ifr.ifr_name, "enp3s0", IFNAMSIZ);
-
-    if (setsockopt(sock_raw, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(ifr)) <
-        0) {
-      perror("setsockopt SO_BINDTODEVICE");
+    // Включаем IP_HDRINCL для ручного контроля над IP заголовком
+    int on = 1;
+    if (setsockopt(sock_raw, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) < 0) {
+      perror("setsockopt IP_HDRINCL");
       return false;
     }
 
@@ -67,15 +63,35 @@ class ICMPMacResolver {
   }
 
   bool send_icmp_request(const char* ip_address) {
-    char packet[IP_MAXPACKET];
+    char packet[sizeof(struct iphdr) + sizeof(struct icmphdr)];
     std::memset(packet, 0, sizeof(packet));
 
+    struct iphdr* ip = reinterpret_cast<struct iphdr*>(packet);
     struct icmphdr* icmp =
         reinterpret_cast<struct icmphdr*>(packet + sizeof(struct iphdr));
+
+    // Заполняем IP заголовок
+    ip->ihl = 5;
+    ip->version = 4;
+    ip->tos = 0;
+    ip->tot_len = htons(sizeof(packet));
+    ip->id = htons(getpid());
+    ip->frag_off = 0;
+    ip->ttl = 64;
+    ip->protocol = IPPROTO_ICMP;
+    ip->check = 0;  // Рассчитается ниже
+    ip->saddr = INADDR_ANY;
+    ip->daddr = inet_addr(ip_address);
+
+    // Рассчитываем контрольную сумму IP заголовка
+    ip->check = checksum(ip, sizeof(struct iphdr));
+
+    // Заполняем ICMP заголовок
     icmp->type = ICMP_ECHO;
     icmp->code = 0;
     icmp->un.echo.id = getpid();
     icmp->un.echo.sequence = 1;
+    icmp->checksum = 0;
     icmp->checksum = checksum(icmp, sizeof(struct icmphdr));
 
     struct sockaddr_in dest;
@@ -83,7 +99,7 @@ class ICMPMacResolver {
     dest.sin_family = AF_INET;
     dest.sin_addr.s_addr = inet_addr(ip_address);
 
-    if (sendto(sock_raw, packet, sizeof(struct icmphdr), 0,
+    if (sendto(sock_raw, packet, sizeof(packet), 0,
                reinterpret_cast<struct sockaddr*>(&dest), sizeof(dest)) < 0) {
       perror("sendto");
       return false;
@@ -93,21 +109,41 @@ class ICMPMacResolver {
   }
 
   bool receive_icmp_reply(unsigned char* mac) {
+    // Для получения MAC адреса нам все же нужен AF_PACKET сокет
+    int sock_packet = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (sock_packet < 0) {
+      perror("socket AF_PACKET");
+      return false;
+    }
+
+    struct ifreq ifr;
+    std::memset(&ifr, 0, sizeof(ifr));
+    std::strncpy(ifr.ifr_name, "enp3s0", IFNAMSIZ);
+
+    if (setsockopt(sock_packet, SOL_SOCKET, SO_BINDTODEVICE, &ifr,
+                   sizeof(ifr)) < 0) {
+      perror("setsockopt SO_BINDTODEVICE");
+      close(sock_packet);
+      return false;
+    }
+
     char packet[IP_MAXPACKET];
     struct sockaddr saddr;
     socklen_t saddr_size = sizeof(saddr);
 
     std::memset(packet, 0, sizeof(packet));
     int bytes =
-        recvfrom(sock_raw, packet, sizeof(packet), 0, &saddr, &saddr_size);
+        recvfrom(sock_packet, packet, sizeof(packet), 0, &saddr, &saddr_size);
     if (bytes < 0) {
       perror("recvfrom");
+      close(sock_packet);
       return false;
     }
 
     struct ethhdr* eth = reinterpret_cast<struct ethhdr*>(packet);
     std::memcpy(mac, eth->h_source, 6);
 
+    close(sock_packet);
     return true;
   }
 };
